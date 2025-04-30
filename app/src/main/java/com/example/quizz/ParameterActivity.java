d4cae10d78c9cff633c8556ea0ad9df4262df2d2
Arthur Vogel <arthur.vogel@epita.fr>
2025-04-30 15:20:53 -0700

    innit parameter activity.java

--- app/src/main/java/com/example/quizz/ParameterActivity.java
+++ app/src/main/java/com/example/quizz/ParameterActivity.java
@@ -1,6 +1,7 @@
 package com.example.quizz;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +9,45 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.myapplication.R;
+import com.example.quizz.databinding.ActivityMainBinding;
+import com.example.quizz.databinding.ActivityParameterBinding;
 
 public class ParameterActivity extends AppCompatActivity {
+    ActivityParameterBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityParameterBinding.inflate(getLayoutInflater());
+
+        binding.deleteAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //delete account
+            }
+        });
+
+        binding.restartPointButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //reset point
+            }
+        });
+
+        binding.renameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //rename user
+            }
+        });
+
+        binding.lightModeSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //change the light mode
+            }
+        });
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_parameter);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
