6756d75776d3407eb6f347f90f8cbfd89b3378d1
Sblablo <nathan.chan-lock@epita.fr>
2025-05-05 20:12:45 -0700

    QuestionActivity2

--- app/src/main/java/com/example/quizz/QuestionActivity.java
+++ app/src/main/java/com/example/quizz/QuestionActivity.java
@@ -1,24 +1,43 @@
 package com.example.quizz;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import com.example.quizz.databinding.ActivityQuestionBinding;
+import com.example.quizz.question.QuestionRepository;
+
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class QuestionActivity extends AppCompatActivity {
+    public static final String TOPIC_ID = "topic_id";
+    public int page = 1;
+    public int maxpage = 5;
+    public int topicId = -1;
+
+    private ActivityQuestionBinding binding;
+    private QuestionRepository questionRepository;
+
+    public static Intent QuestionActivityIntentFactory(Context applicationContext, int topicId){
+        Intent intent = new Intent(applicationContext, QuestionActivity.class);
+        intent.putExtra(TOPIC_ID, topicId);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_question);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityQuestionBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        questionRepository = QuestionRepository.getRepository(getApplication());
+        topicId = getIntent().getIntExtra(TOPIC_ID, -1);
+
+        binding.textViewTopic.setText("getTopicStringByIdthatshouldbedonebyArnaud");
+
     }
 }
